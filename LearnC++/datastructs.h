b5396ada7f28c686190233c2a88b6c632dacc3e7
brodiebrenna <brodiebrenna@gmail.com>
2025-02-10 20:56:01 -0700

    I fixed the weird off by 1 error in deleteByIndex. It has passed a manual unit test though I do want to write some more thorough unit tests to verify everything. However right now I am seeing an error in the reverse method so I am going to fix that before going onto building concrete unit tests

--- LearnC++/datastructs.h
+++ LearnC++/datastructs.h
@@ -9,42 +9,144 @@
 
 */
 
+namespace learncpp {
 
-template<typename DataType>
-struct SinglyLinkedNode {
-	DataType data;
-	std::unique_ptr<SinglyLinkedNode<DataType>> nextNode;
 
-	SinglyLinkedNode(DataType value) : data(value), nextNode(nullptr) {}
-};
+	template<typename DataType>
+	struct SinglyLinkedNode {
+		DataType data;
+		std::unique_ptr<SinglyLinkedNode<DataType>> nextNode;
 
-template<typename DataType>
-class LinkedList {
-private:
-	std::unique_ptr<SinglyLinkedNode<DataType>> head;
-public:
-	LinkedList() : head(nullptr) {}
+		SinglyLinkedNode(DataType value) : data(value), nextNode(nullptr) {}
+	};
 
-	void insertAtHead(DataType value) {
-		std::unique_ptr<SinglyLinkedNode<DataType>> newNode = make_unique<SinglyLinkedNode<DataType>>(value);
-		newNode->nextNode = move(head);
-		head = move(newNode);
-	}
+	template<typename DataType>
+	class LinkedList {
+	private:
+		std::unique_ptr<SinglyLinkedNode<DataType>> head;
+		int numberOfElements = 0;
+	public:
+		LinkedList() : head(nullptr) {}
 
-	void insertAtEnd(DataType value) {
-		std::unique_ptr<SinglyLinkedNode<DataType>> newNode = make_unique<SinglyLinkedNode<DataType>>(value);
-		if (!head) {
-			head = move(newNode);
-			return;
+		int size() {
+			return numberOfElements;
 		}
-		SinglyLinkedNode<DataType>* temp = head.get();
-		while (temp->nextNode) {
-			temp = temp->nextNode.get();
+
+		void insertAtHead(DataType value) {
+			std::unique_ptr<SinglyLinkedNode<DataType>> newNode = std::make_unique<SinglyLinkedNode<DataType>>(value);
+			newNode->nextNode = std::move(head);
+			head = std::move(newNode);
+			numberOfElements++;
 		}
-		temp -> nextNode = move(newNode)
-	}
-	void deleteByValye(DataType value) {
 
-	}
+		void insertAtEnd(DataType value) {
+			std::unique_ptr<SinglyLinkedNode<DataType>> newNode = std::make_unique<SinglyLinkedNode<DataType>>(value);
+			if (!head) {
+				head = std::move(newNode);
+				numberOfElements++;
+				return;
+			}
+			SinglyLinkedNode<DataType>* temp = head.get();
+			while (temp->nextNode) {
+				temp = temp->nextNode.get();
+			}
+			temp->nextNode = std::move(newNode);
+			numberOfElements++;
+		}
+		void deleteHead() {
+			if (!head) {
+				return;
+			}
+			if (head->nextNode) {
+				head = std::move(head->nextNode);
+			}
+			else {
+				head.reset();
+			}
+			numberOfElements--;
+		}
+		void deleteFromEnd(){
+			if (!head) {
+				return;
+			}
+			if (!head->nextNode) {
+				deleteHead();
+				return;
+			}
+			SinglyLinkedNode<DataType>* temp = head.get();
+			while (temp->nextNode->nextNode) {
+				temp = temp->nextNode.get();
+			}
+			temp->nextNode.reset();
+			numberOfElements--;
+		}
+		void deleteByValue(DataType valueToDelete) {
+			if (!head) {
+				return;
+			}
+			if (head->data == valueToDelete) {
+				deleteHead();
+				return;
+			}
+			SinglyLinkedNode<DataType>* temp = head.get();
+			while (temp->nextNode && temp->nextNode->data != valueToDelete) {
+				temp = temp->nextNode.get();
+			}
+			if (temp->nextNode) {
+				temp->nextNode = std::move(temp->nextNode->nextNode);
+			}
+			numberOfElements--;
+		}
 
-};
+		void deleteByIndex(int index) {
+			if (!head) {
+				return;
+			}
+			if (index == 0) {
+				deleteHead();
+				return;
+			}
+			int counter = 1;
+			SinglyLinkedNode<DataType>* temp = head.get();
+			while (counter < index && temp->nextNode) {
+				temp = temp->nextNode.get();
+				counter++;
+			}
+			if (temp->nextNode) {
+				temp->nextNode = std::move(temp->nextNode->nextNode);
+			}
+			numberOfElements--;
+		}
+
+		void reverse() {
+			std::unique_ptr<SinglyLinkedNode<DataType>> previousNode = nullptr;
+			std::unique_ptr<SinglyLinkedNode<DataType>> currentNode = std::move(head);
+
+			while (currentNode) {
+				std::unique_ptr<SinglyLinkedNode<DataType>> next = std::move(currentNode->nextNode);
+				currentNode->nextNode = std::move(previousNode);
+				currentNode = std::move(next);
+			}
+			head = std::move(previousNode);
+
+		}
+
+		DataType getValueByIndex(int index) {
+			if (!head) {
+				return NULL;
+			}
+			if (index > size()) {
+				return NULL;
+			}
+
+			SinglyLinkedNode<DataType>* temp = head.get();
+			int counter = 0;
+			while (counter < index && temp->nextNode) {
+				temp = temp->nextNode.get();
+				counter++;
+			}
+			return temp->data;
+
+		}
+	};
+}
--- LearnC++/main.cpp
+++ LearnC++/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "datastructs.h"
+
+void printLinkedList(learncpp::LinkedList<int>& linkedlist) {
+	int size = linkedlist.size();
+	for (int i = 0; i < size; i++) {
+		std::cout << linkedlist.getValueByIndex(i);
+	}
+	std::cout << std::endl;
+}
+
+void LinkedListTest() {
+	int data[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	learncpp::LinkedList<int> linkedlist;
+	for (int iterator = 0; iterator < 9; iterator++) {
+		linkedlist.insertAtEnd(data[iterator]);
+	}
+	std::cout << linkedlist.size() << std::endl;
+	printLinkedList(linkedlist);
+	linkedlist.deleteByIndex(3);
+	printLinkedList(linkedlist);
+	linkedlist.deleteByIndex(2);
+	printLinkedList(linkedlist);
+	linkedlist.deleteByIndex(1);
+	printLinkedList(linkedlist);
+	linkedlist.deleteByIndex(0);
+	printLinkedList(linkedlist);
+	
+}
 
 int main() {
-	std::cout << "Hello" << std::endl;
+	LinkedListTest();
 	return 0;
 }
